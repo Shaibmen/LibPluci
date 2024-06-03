571b95330bca77c82a75eed4aabe50a0df2e80eb
Shaibmen <ibin2005636@gmail.com>
2024-06-03 19:17:33 +0300

    ....exxexexexexexexexexe

---
+++ DynamicLib.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+__declspec(dllexport) bool LetterINWord(char letter, string word)
+{
+    string str = word;
+    int count = 0;
+    for (int i = 0; i <= str.length(); i++)
+    {
+        if (str[i] == letter) {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        cout << "Bykva no likvid" << endl;
+        return false;
+    }
+    else if (count > 0)
+    {
+        cout << "there is " + to_string(count) + " bykva " + letter + " imeetsa " + str << endl;
+        return true;
+    }
+}
---
+++ Header.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+__declspec(dllexport) bool LetterINWord(char letter, string);
